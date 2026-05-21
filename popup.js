99f9f42518387dee92cd78ab83fd63f6d90ade75
Your Name <you@example.com>
2026-05-21 18:48:36 +0700

    feat: create popup UI

---
+++ popup.js
@@ -0,0 +1,4 @@
+document.getElementById('fillButton').addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  chrome.tabs.sendMessage(tab.id, {action: "start_fill"});
+});
